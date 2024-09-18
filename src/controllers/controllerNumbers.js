be9ba3dd0b839c8a9c68fcb09425a27f316807df
prieloizee <priscila.e.paulino@aluno.senai.br>
2024-09-18 09:40:29 -0300

    18/09

---
+++ src/controllers/controllerNumbers.js
@@ -0,0 +1,26 @@
+module.exports = class controllerNumbers{
+    static async checkpar(req, res){
+        const{numero} = req.body;
+
+        if(numero % 2 == 0){
+            return res.status(200).json({message:"Número Par!!"});
+        }
+        return res.status(200).json({message:"Número Impar!!"});
+    }
+    
+    static async checkPrimo(req,res){
+        const{numero} = req.body;
+
+        if(numero <=1){
+            return res.status(200).json({message:"Não é Primo!!"});
+        }
+
+        for(let i = 2; i < numero; i++){
+            if(numero % i == 0){
+                return res.status(200).json({message:"Não é Primo!!"});
+            }
+        }
+
+        return res.status(200).json({message:"É Primo!!"});
+    }
+}
