68627b65fdd2be1a644e87c0d0c4a4227698d074
Priscila <priscila.e.paulino@aluno.senai.br>
2024-09-18 12:52:19 -0300

    Update controllerNumbers.js

--- src/controllers/controllerNumbers.js
+++ src/controllers/controllerNumbers.js
@@ -1,6 +1,11 @@
 module.exports = class controllerNumbers{
     static async checkpar(req, res){
         const{numero} = req.body;
+        
+        if(typeof numero !=="number"){
+            return res.status(400).json({message:"Não é um número!!!"});
+        }
+
 
         if(numero % 2 == 0){
             return res.status(200).json({message:"Número Par!!"});
